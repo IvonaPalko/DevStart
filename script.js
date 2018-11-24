4411f88a51e04b74eb3c9db015172e9ae2283886
Ivona Palko <ivona.palko@gmail.com>
2018-11-24 15:17:14 +0100

    adding js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let mainNav = document.getElementById("js-menu");
-let navBarToggle =document.getElementById("js-navbar-toggle");
+let navBarToggle = document.getElementById("js-navbar-toggle");
 
 navBarToggle.addEventListener("click", function() {
-    mainNav.classList.toggle("active");
+  mainNav.classList.toggle("active");
 });
